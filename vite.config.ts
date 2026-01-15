a8a5e1405ac379800217615b554464c93890cdfd
Docs <arya@MacBook-Pro.local>
2026-01-15 19:57:13 +0700

    fix: port host on vite vconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
@@ -13,4 +15,4 @@ export default defineConfig({
       },
     },
   },
-});
+})
